7fc2c274c2ae1e4a535c4d838560c53bb6d8fcbd
Андрей <andrdog@MacBook-Pro-1.IGD_Rostelecom>
2025-01-13 15:58:48 +0300

    Четвертое задание. Рефакторинг и создание структуры

--- internal/taskService/orm.go
+++ internal/taskService/orm.go
@@ -1,9 +1,9 @@
-package main
+package taskService
 
 import "gorm.io/gorm"
 
 // Определяем структуру Message, которая будет представлять таблицу в базе данных
-type Message struct {
+type Task struct {
 	gorm.Model        // По сути, прописывая gorm.Model, мы просто добавляем поля ID, CreatedAt, UpdatedAt и DeletedAt
 	Task       string `json:"task"`    // Наш сервер будет ожидать json c полем text
 	IsDone     bool   `json:"is_done"` // В GO используем CamelCase, в Json - snake
